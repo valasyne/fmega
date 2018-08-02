1ac918afa80e8849ddca610002f8091cfcac1504
vl <vasil.deville@gmail.com>
2018-08-02 23:20:23 +0300

    Add the logger and common gmail params

--- config.py
+++ config.py
@@ -7,3 +7,9 @@ class Config(object):
     'sqlite:///' + os.path.join(basedir, 'app.db')
     # Don't signal the app every time a change is abt to be made in the db
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['valasema@yahoo.com']
